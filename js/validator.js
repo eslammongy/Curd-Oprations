26650008216eca47e011a0656c0d8001bbfd91aa
eslam <eslammongy995@gmail.com>
2022-09-14 01:34:33 +0200

    modifying html structure and js code

--- js/validator.js
+++ js/validator.js
@@ -1,10 +1,9 @@
 function validate(val) {
-    v1 = document.getElementById("fname");
-    v2 = document.getElementById("lname");
-    v3 = document.getElementById("email");
-    v4 = document.getElementById("mob");
-    v5 = document.getElementById("job");
-    v6 = document.getElementById("ans");
+    userName = document.getElementById("user_name");
+    userEmail = document.getElementById("email");
+    userPhone = document.getElementById("user_phone");
+    jopTitle = document.getElementById("jop_title");
+    aboutUser = document.getElementById("about_friend");
 
     flag1 = true;
     flag2 = true;
@@ -14,63 +13,52 @@ function validate(val) {
     flag6 = true;
 
     if (val >= 1 || val == 0) {
-        if (v1.value == "") {
-            v1.style.borderColor = "red";
+        if (userName.value == "") {
+            userName.style.borderColor = "red";
             flag1 = false;
         }
         else {
-            v1.style.borderColor = "green";
+            userName.style.borderColor = "green";
             flag1 = true;
         }
     }
-
-    if (val >= 2 || val == 0) {
-        if (v2.value == "") {
-            v2.style.borderColor = "red";
-            flag2 = false;
-        }
-        else {
-            v2.style.borderColor = "green";
-            flag2 = true;
-        }
-    }
     if (val >= 3 || val == 0) {
-        if (v3.value == "") {
-            v3.style.borderColor = "red";
+        if (userEmail.value == "") {
+            userEmail.style.borderColor = "red";
             flag3 = false;
         }
         else {
-            v3.style.borderColor = "green";
+            userEmail.style.borderColor = "green";
             flag3 = true;
         }
     }
     if (val >= 4 || val == 0) {
-        if (v4.value == "") {
-            v4.style.borderColor = "red";
+        if (userPhone.value == "") {
+            userPhone.style.borderColor = "red";
             flag4 = false;
         }
         else {
-            v4.style.borderColor = "green";
+            userPhone.style.borderColor = "green";
             flag4 = true;
         }
     }
     if (val >= 5 || val == 0) {
-        if (v5.value == "") {
-            v5.style.borderColor = "red";
+        if (jopTitle.value == "") {
+            jopTitle.style.borderColor = "red";
             flag5 = false;
         }
         else {
-            v5.style.borderColor = "green";
+            jopTitle.style.borderColor = "green";
             flag5 = true;
         }
     }
     if (val >= 6 || val == 0) {
-        if (v6.value == "") {
-            v6.style.borderColor = "red";
+        if (aboutUser.value == "") {
+            aboutUser.style.borderColor = "red";
             flag6 = false;
         }
         else {
-            v6.style.borderColor = "green";
+            aboutUser.style.borderColor = "green";
             flag6 = true;
         }
     }
