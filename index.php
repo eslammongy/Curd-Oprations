99ce8212ebdb251d0fe716d12ce50d9c3e71428c
eslam <eslammongy995@gmail.com>
2022-09-15 01:30:39 +0200

    inserting new friend in database

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
-include('saving_friend.php');
 
+error_reporting(E_ERROR | E_PARSE);
+include('./config/config.php');
 
+if (isset($_POST['saveFriend'])) {
+
+  $friendName = $_POST['user_name'];
+  $friendPhone = $_POST['user_phone'];
+  $friendEmail = $_POST['email'];
+  $jobTitle = $_POST['jop_title'];
+  $aboutFriend = $_POST['about_friend'];
+
+  $insertingQuery = "INSERT INTO users(userName,userEmail,jobTitle,userPhone,aboutUser) VALUES('$friendName','$friendEmail','$jobTitle','$friendPhone','$aboutFriend')";
+  $saveNewFriend = mysqli_query($db_connection, $insertingQuery) or die('error occured when adding new product');
+
+  if ($saveNewFriend) {
+
+    $message[] = "Saving new friend successfully";
+    // header('location:index.php');
+  } else {
+
+    $message[] = "Feild when save new friend successfully";
+    //header('location:index.php');
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,16 +38,24 @@ include('saving_friend.php');
     <link href="css/style.css" rel="stylesheet" />
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
 <body>
+    <?php
+  if (isset($message)) {
+    foreach ($message as $message) {
+      echo '<div class="message"><span>' . $message . '</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+    }
+  };
+  ?>
     <div class="container-fluid px-1 py-5 mx-auto">
         <div class="row d-flex justify-content-center">
             <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
                 <div class="card">
                     <h5 class="text-center mb-4">Saving new friend in your list</h5>
-                    <form class="form-card" action="" method="post" onsubmit="event.preventDefault()">
+                    <form class="form-card" action="" method="POST">
                         <div class="row justify-content-between text-left">
                             <div class="form-group col-sm-6 flex-column d-flex">
                                 <label class="form-control-label px-3">name<span class="text-danger"> *</span></label>
@@ -63,7 +93,7 @@ include('saving_friend.php');
                         </div>
                         <div class="row justify-content-end">
                             <div class="form-group col-sm">
-                                <button type="submit" class="btn-block btn-primary">
+                                <button type="submit" name="saveFriend" class="btn-block btn-primary">
                                     Save
                                 </button>
                             </div>
--- saving_friend.php
+++ saving_friend.php
@@ -2,3 +2,21 @@
 
 error_reporting(E_ERROR | E_PARSE);
 include('config.php');
+
+if (isset($_POST['saveFriend'])) {
+
+    $friendName = $_POST['user_name'];
+    $friendPhone = $_POST['user_phone'];
+    $friendEmail = $_POST['email'];
+    $jobTitle = $_POST['jop_title'];
+    $aboutFriend = $_POST['about_friend'];
+
+    $insertingQuery = "INSERT INTO users(userName,userEmail,jobTitle,userPhone,userEmail,aboutUser) VALUES('$friendName','$friendEmail','$jobTitle','$friendPhone','$aboutFriend')";
+    $saveNewFriend = mysqli_query($db_connection, $insertingQuery) or die('error occured when adding new product');
+
+    if ($saveNewFriend) {
+        $message[] = "Saving new friend successfully";
+    } else {
+        $message[] = "Feild when save new friend successfully";
+    }
+}
